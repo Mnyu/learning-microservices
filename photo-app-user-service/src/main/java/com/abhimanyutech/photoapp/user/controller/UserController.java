b330160243d04d4495391bad561ca1eabdb3ee80
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2022-02-17 19:00:54 +0530

    adding support to accept/return xml or json

--- photo-app-user-service/src/main/java/com/abhimanyutech/photoapp/user/controller/UserController.java
+++ photo-app-user-service/src/main/java/com/abhimanyutech/photoapp/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.abhimanyutech.photoapp.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +36,8 @@ public class UserController {
         return "WORKING on port : " + env.getProperty("local.server.port");
     }
 
-    @PostMapping
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+        produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<UserResponseTO> createUser(@Valid @RequestBody UserTO userTO) {
         UserDTO userDTO = userService.createUser(UserDTO.builder()
                 .email(userTO.getEmail())
